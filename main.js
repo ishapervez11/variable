9e8b9e7080b019642c5959e33167964f21606597
Isha Pervez <162013997+ishapervez11@users.noreply.github.com>
2024-03-14 11:45:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var name1 = "isha pervez";
+var age = 17;
+var isMarried = false;
+console.log(name1);
+console.log(age);
+console.log(isMarried);
---
+++ main.ts
@@ -0,0 +1,7 @@
+let name1:string="isha pervez"
+let age:number=17
+let isMarried:boolean=false
+
+console.log(name1)
+console.log(age)
+console.log(isMarried)
